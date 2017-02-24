17307ade47668656d2ef66bb094329963412c5c4
Neeraj Moudgil <neerajcse7@gmail.com>
2017-02-24 18:12:32 +0530

    Added GCE library Part-1

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,7 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -43,8 +45,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
+
+        new EndPointAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+    }
+    public void displayJoke(View view) {
        // Toast.makeText(this, Joke.getJoke(), Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, JokeActivity.class);
+       Intent intent = new Intent(this, JokeActivity.class);
         intent.putExtra(JokeActivity.JOKEINTENT, Joke.getJoke());
         startActivity(intent);
     }
